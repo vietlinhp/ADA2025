189fb1548e32cf188f0debd0c4bfdc0990e1ba4f
Indika Kumara <ipkumarawd@yahoo.com>
2025-02-26 14:38:27 +0100

    Updated livelab example

--- lab4/livelab/deliveryservice_faas/tests/test_add.py
+++ lab4/livelab/deliveryservice_faas/tests/test_add.py
@@ -12,7 +12,6 @@ Base.metadata.create_all(engine)
 # 3 - create a new session
 session = Session()
 
-
 status_1 = StatusDAO(STATUS_CREATED, datetime.datetime.now())
 delivery_1 = DeliveryDAO("cus_1", "thaifood", "pack1", datetime.datetime.now(), datetime.datetime(2021, 3, 17, 23, 45),
                          status_1)
