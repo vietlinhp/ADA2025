6db974691e54b5bc8ad96702a435024774931767
vietlinh <v.l.pham@tilburguniversity.edu>
2025-03-21 11:27:11 +0100

    lab6live

--- lab6/choreography/user/create_topic_sub.py
+++ lab6/choreography/user/create_topic_sub.py
@@ -4,14 +4,14 @@ from user_consumer import create_subscription
 from user_publisher import create_topic
 
 logging.getLogger().setLevel(logging.INFO)
-create_topic("your_project_id", "order_req") # make sure to change the project id
-create_subscription("your_project_id", "order_req", "order_req_sub")
-create_topic("your_project_id", "inventory_status")
-create_topic("your_project_id", "order_status")
-create_topic("your_project_id", "order_status_user")
-create_subscription("your_project_id", "order_status_user", "order_status_user_sub")
-create_topic("your_project_id", "order_req")
-create_topic("your_project_id", "inventory_status")
-create_subscription("your_project_id", "inventory_status",
+create_topic("ada24-449918", "order_req") # make sure to change the project id
+create_subscription("ada24-449918", "order_req", "order_req_sub")
+create_topic("ada24-449918", "inventory_status")
+create_topic("ada24-449918", "order_status")
+create_topic("ada24-449918", "order_status_user")
+create_subscription("ada24-449918", "order_status_user", "order_status_user_sub")
+create_topic("ada24-449918", "order_req")
+create_topic("ada24-449918", "inventory_status")
+create_subscription("ada24-449918", "inventory_status",
                     "inventory_status_sub")
-create_subscription("your_project_id", "order_status","order_status_sub")
+create_subscription("ada24-449918", "order_status","order_status_sub")
